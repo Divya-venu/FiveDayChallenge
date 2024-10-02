fe0179d60b56641257c4378c88c21d02f7f483b3
Divya-venu <138234922+Divya-venu@users.noreply.github.com>
2024-10-02 13:40:41 -0400

    Add files via upload

--- Program1.java
+++ Program1.java
@@ -1,42 +1,35 @@
 package com.FDC;
 import java.util.Scanner;
-public class Program1
-{
-	
-	public static void main(String[] args)
-	{
-		
-			Scanner scan =new Scanner(System.in);
-			System.out.println("First number: ");
-			int num1=scan.nextInt();
-			System.out.println("Second number: ");
-			int num2=scan.nextInt();
-			System.out.println("Third number: ");
-			scan.close();
-			int num3=scan.nextInt();
-			
-			int greater;
-			if(num1>=num2 && num1>=num3)
-			{
-				greater=num1;
-			}
-			else if(num2>=num1 && num2>=num3)
-			{
-				greater=num2;
-			}
-			else
-			{
-				greater=num3;
-			}
-			System.out.println("The Greater number is: "+greater);
-	}
+public class Program1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of lines:");
+        int n = scanner.nextInt();
+        scanner.close();
+        for (int i = 1; i <= n; i++) {
+          
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+           
+            for (int k = 1; k <= i; k++) {
+                if (k == 1 || k == i || i == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
-			
+
+
+				
+		
 		
 
 	
 
-
 	
 
-	
--- Program2.java
+++ Program2.java
@@ -1,48 +1,37 @@
 package com.FDC;
-
 import java.util.Scanner;
-
 public class Program2 {
-	public static void main(String[] args)
-	{
-		Scanner scan = new Scanner(System.in);
-		        System.out.println("Enter the year:");
-		        
-		        int year = scan.nextInt();
-		        scan.close();
-		        boolean LeapYear = false;
-		        if (year % 4 == 0) 
-		        {
-		            if (year % 100 == 0) 
-		            {
-		                if (year % 400 == 0) 
-		                {
-		                    LeapYear = true;
-		                } 
-		                else 
-		                {
-		                    LeapYear = false;
-		                }
-		            }
-		            else 
-		            {
-		                LeapYear = true;
-		            }
-		        } 
-		        else 
-		        {
-		            LeapYear = false;
-		        }		        
-		        if (LeapYear)
-		        {
-		            System.out.println("True");
-		        } else 
-		        {
-		            System.out.println("False");
-		        }		 
-		        
-		}
+			
+	
+	
+	    public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
 
-	}
+	       
+	        System.out.print(" first integer: ");
+	        int firstInt = scan.nextInt();
+	        System.out.print(" second integer: ");
+	        
+	        int secondInt = scan.nextInt();
+	        scan.close();
+	       
+	        char firstChar = getUnicodeCharacter(firstInt);
+	        char secondChar = getUnicodeCharacter(secondInt);
+
+	        
+	        System.out.println("The corresponding characters are: " + firstChar + " " + secondChar);
+	    }
+
+	   
+	    public static char getUnicodeCharacter(int codePoint) {
+	        return (char) codePoint;
+	    }
+	
+
+
+			}
+
+		
+	
 
 
